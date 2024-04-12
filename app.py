c2e4b90444283722e47f3d865a1eb98055cd35a7
fayadchowdhury <fayad.chowdhury@gmail.com>
2024-04-12 16:20:48 -0700

    Write a consolidated script to run the project.

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from data import data_script
+from elo_rating import elo_rating
+from outcome_classifier import model
 
 if __name__=="__main__":
     # TODO: Write and run cleanup script if possible
     data_script.run()
+    # Next run Elo ratings scripts
+    elo_rating.run("data/data_output/combined.csv")
+    # Next run model code
+    model.run()
