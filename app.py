e5c85503041a90ca9cf27f0b57123441f654467f
fayadchowdhury <fayad.chowdhury@gmail.com>
2024-04-12 15:02:22 -0700

    Start basic app script.

---
+++ app.py
@@ -0,0 +1,10 @@
+# Main app code
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from data import data_script
+
+if __name__=="__main__":
+    # TODO: Write and run cleanup script if possible
+    data_script.run()
