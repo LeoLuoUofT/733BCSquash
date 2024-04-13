1fd16cf45cac959938cd4865eb301c3d2aa3d049
fayadchowdhury <fayad.chowdhury@gmail.com>
2024-04-13 00:11:24 -0700

    Update the requirements.txt and serve the Flask app from the main app itself.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ warnings.filterwarnings("ignore")
 from data import data_script
 from elo_rating import elo_rating
 from outcome_classifier import model
+from eloApp import app
 
 if __name__=="__main__":
     # TODO: Write and run cleanup script if possible
@@ -14,3 +15,5 @@ if __name__=="__main__":
     elo_rating.run("data/data_output/combined.csv")
     # Next run model code
     model.run()
+    # Run frontend
+    app.run()
